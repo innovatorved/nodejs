bded56da92442645f6a84e5703a386fbaadd91e6
Next In <vedgupta@protonmail.com>
2021-08-27 11:48:53 +0000

    send mail using nodejs

---
+++ n6-nodemailer.js
@@ -0,0 +1,26 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'youremail@gmail.com',
+    pass: 'yourpassword'
+  }
+});
+
+var mailOptions = {
+  from: 'youremail@gmail.com',
+  to: 'myfriend@yahoo.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+  // we aso send HTML by adding html key 
+  // html : <h1>This is HTML</h1>
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+}); 
